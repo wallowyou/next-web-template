db1997784576c7ab184ed2d610a5ae67e1d4ad8f
jgp <jiangguiping@shensizaowu.com>
2026-02-06 16:12:37 +0800

    feat: 增加eslint配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,76 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+// @ts-check
+import antfu from '@antfu/eslint-config'
+import tailwindcss from 'eslint-plugin-better-tailwindcss'
+import sonar from 'eslint-plugin-sonarjs'
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+export default antfu(
+  {
+    react: {
+      // This react compiler rules are pretty slow
+      // We can wait for https://github.com/Rel1cx/eslint-react/issues/1237
+      reactCompiler: false,
+      overrides: {
+        'react/no-context-provider': 'off',
+        'react/no-forward-ref': 'off',
+        'react/no-use-context': 'off',
 
-export default eslintConfig;
+        // prefer react-hooks-extra/no-direct-set-state-in-use-effect
+        'react-hooks/set-state-in-effect': 'off',
+        'react-hooks-extra/no-direct-set-state-in-use-effect': 'error',
+      },
+    },
+    nextjs: true,
+    ignores: ['public'],
+    typescript: {
+      overrides: {
+        'ts/consistent-type-definitions': ['error', 'type'],
+        'ts/no-explicit-any': 'error',
+      },
+    },
+    test: {
+      overrides: {
+        'test/prefer-lowercase-title': 'off',
+      },
+    },
+    stylistic: {
+      overrides: {
+        'antfu/top-level-function': 'off',
+      },
+    },
+  },
+  {
+    rules: {
+      'node/prefer-global/process': 'off',
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    settings: {
+      'react-x': {
+        additionalStateHooks: '/^use\\w*State(?:s)?|useAtom$/u',
+      },
+    },
+  },
+  // sonar
+  {
+    rules: {
+      // Manually pick rules that are actually useful and not slow.
+      // Or we can just drop the plugin entirely.
+    },
+    plugins: {
+      sonarjs: sonar,
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      tailwindcss,
+    },
+    rules: {
+      'tailwindcss/enforce-consistent-class-order': 'error',
+      'tailwindcss/no-duplicate-classes': 'error',
+      'tailwindcss/no-unnecessary-whitespace': 'error',
+      'tailwindcss/no-unknown-classes': 'warn',
+    },
+  },
+)
